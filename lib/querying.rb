48d38474d59e3e43892c99aef2bc682a7124142e
Hannah Angle <hannahcangle@gmail.com>
2020-09-04 21:01:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,18 +28,19 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  <<-SQL 
-  SELECT authors.name, subgenres.name 
-  FROM authors
-  JOIN series 
-  ON series.author_id = aithors.id 
-  JOIN subgenres 
-  ON series.subgenre_id = sungenre.id 
+  <<-SQL
+    SELECT authors.name, subgenres.name 
+    FROM authors 
+    JOIN series 
+    ON series.author_id = authors.id 
+    JOIN subgenres 
+    ON series.subgenre_id = subgenres.id
   SQL
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  <<-SQL 
+  SELECT series.title
 end
 
 def select_character_names_and_number_of_books_they_are_in
