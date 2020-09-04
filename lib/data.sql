79511d3043f38830ba66fa03ddcf340c78ca3993
Hannah Angle <hannahcangle@gmail.com>
2020-09-04 21:14:29 +0000

    Automatically backed up by Learn

--- lib/data.sql
+++ lib/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Harry Potter", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "fantasy");
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
-INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "JKR");
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "Philosophers Stone", 2002, 2), (5, "Chamber of Secrets", 2003, 2), (6, "Prisoner of Azkaban", 2005, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "human", 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Harry Potter", "Expelliarmus", "wizard", 2), (6, "Hermone Granger", "Wingardium Leviosa", "witch", 2), (7, "Ron Weasley", "bloody hell", "wizard", 2), (8, "Hagrid", "Better out than in", "half giant", 2);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
