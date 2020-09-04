84f4e14191c67f269ca1aa59201457e3e5f88e36
Hannah Angle <hannahcangle@gmail.com>
2020-09-04 21:03:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   <<-SQL
-  SELECT title, year
-  FROM books 
-  WHERE series_id=1 
-  ORDER BY year 
- SQL
+    SELECT title, year 
+    FROM books 
+    WHERE series_id=1 
+    ORDER BY year
+  SQL
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   <<-SQL
-  SELECT name, motto 
-  FROM characters 
-  ORDER BY LENGTH(motto) DESC 
-  LIMIT 1 
+    SELECT name, motto 
+    FROM characters 
+    ORDER BY LENGTH(motto) DESC 
+    LIMIT 1
   SQL
 end
 
-
 def select_value_and_count_of_most_prolific_species
-   <<-SQL
+  <<-SQL
     SELECT species, COUNT(*) 
     FROM characters 
     GROUP BY species 
@@ -39,7 +38,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-<<-SQL
+  <<-SQL
     SELECT series.title
     FROM series
     JOIN books
@@ -56,7 +55,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-   <<-SQL
+  <<-SQL
     SELECT characters.name, COUNT(*) as book_count 
     FROM character_books 
     JOIN characters 
